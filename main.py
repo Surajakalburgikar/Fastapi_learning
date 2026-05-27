19b79d11fa02d16af1cf10a56270647ba0851d3e
Surajakalburgikar <surajak808@gmail.com>
2026-05-27 22:40:37 +0530

    new main file for testing database

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from src.utils.db import Base,engine
 
-Base.metadata.create_all(engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title = "this is my tak managament application")
+
+@app.get("/")
+def home():
+    return {"message": "hello"}
